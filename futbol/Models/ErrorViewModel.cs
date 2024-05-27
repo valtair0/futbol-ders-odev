dad3cda2cb2beb86e185d70bec73cc356a070408
valtair <valtair@DESKTOP-51SGEC3>
2024-05-27 22:52:42 +0300

    Add project files.

---
+++ futbol/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace futbol.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
